403daf92b2cb3ad1506a9a8a1b20a32e04fed665
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-11 16:39:26 -0700

    try1 task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+	int j = 1;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10 ; j++)
+		{
+			if (j > i)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 8 && j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar ('\n');
+	return (0);
+}
