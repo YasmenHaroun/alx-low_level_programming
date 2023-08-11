effb0f688d853ee2f711b010ed43e3b83093fd3d
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-11 16:49:06 -0700

    try2 task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,7 +18,11 @@ int main(void)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
-				if (i != 8 && j != 9)
+				if (i == 8 && j == 9)
+				{
+					continue;
+				}
+				else
 				{
 					putchar(',');
 					putchar(' ');
