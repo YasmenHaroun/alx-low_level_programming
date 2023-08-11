ae79d2495e30e35b53ac4ef573278a7a1af2ea59
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-11 09:53:46 -0700

    task8 try2

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,12 @@ int main(void)
 {
 	int i;
 	char c;
+	
 	for (i = 0; i < 10; i++)
 	{
 		putchar((i % 10) + '0');
 	}
-	for (c = 'a'; c <= 'E'; c++)
+	for (c = 'a'; c =< 'E'; c++)
 	{
 		putchar(c);
 	}
