4562833dacd939b909628ed1cd0bd2dc298542c7
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-11 05:02:30 -0700

    try1 task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints digits from 0to9
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
