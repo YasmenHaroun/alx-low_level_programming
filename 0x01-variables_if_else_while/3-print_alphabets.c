e3b58dca7cfc96a5f591099d8b7000bcaf0b785b
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-11 04:36:10 -0700

    try1 task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints letters lower and upper
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c, C;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (C = 'A'; C <= 'Z'; C++)
+	{
+	putchar(C)
+	}
+
+	putchar('\n')
+
+	return (0);
+}
