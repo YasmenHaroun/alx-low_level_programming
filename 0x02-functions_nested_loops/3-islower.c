aab91358fa931be39b95a341973a27d8165ee29b
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-15 04:38:30 -0700

    try1 task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * int _islower - checks for lowercase character
+  * 
+  * Return: int
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	
+	return (0);
+	
+}
