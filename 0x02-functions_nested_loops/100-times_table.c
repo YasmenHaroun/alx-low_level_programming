e4b3524f281c64d9bb50d497912235f82961f6ab
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-15 13:03:24 -0700

    try1 task12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include " main.h"
+
+/**
+ * print_times_table - print the time table till n
+ * @n: input limit of time table
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if ((i * j) < 10)
+			{
+				_putchar(' ');
+				_putchar((i * j) + '0');
+			}
+			else
+			{
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
