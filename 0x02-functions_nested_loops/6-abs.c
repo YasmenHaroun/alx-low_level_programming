16c130d1e8d653a6dd62652d83861ce0eca8574b
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-15 07:13:06 -0700

    try1 task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _abs - find absolute of integer
+ *
+ * @n: intger input
+ *
+ * Return: positive intger.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+
+	return (n);
+}
