f44cc9c4de323ee57e7a740938676f50b9d271fb
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-16 00:09:40 -0700

    try2 task16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,21 +8,16 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1, k = 2;
+	long previous = 1, current = 2;
+	long sum = 0;
 
+	printf("1, 2, ");
 	while (i < 96)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
-
+		sum = previous + current;
+		previous = current;
+		current = sum;
+		printf("%ld, ", sum);
 		++i;
 	}
 
