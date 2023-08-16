2a3239c0c52779dc2379879452e8bc9cb406173a
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-16 08:59:27 -0700

    try4 task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,13 +24,16 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		/*if (month == 2 && day == 60)
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaing days: %d\n", 365 - day);
-		}
-		else
-		{*/
+		/*
+		* if (month == 2 && day == 60)
+		*{
+		*	printf("Day of the year: %d\n", day);
+		*	printf("Remaing days: %d\n", 365 - day);
+		*}
+		*else
+		*{
+		*/
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
 		/*}*/
