a6f7c26a66d503d3308ce41f0c0c9a5ef26f0fff
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-17 05:10:41 -0700

    try1 task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "stdio"
+
+/**
+ * fizzbuzz - program that prints the numbers till 100
+ *
+ * Return: void
+ */
+int fizzbuzz(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
