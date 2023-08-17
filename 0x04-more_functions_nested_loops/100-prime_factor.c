b8c26395f26c1b7d00f876ada85cff6d091ca0c6
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-17 15:41:27 -0700

    task11 try12

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,16 +9,15 @@
 int main(void)
 {
 	long n = 612852475143;
-	long m = 612852475143;
-	long i = 3;
+	int i = 3;
 
 	while (i < n)
 	{
 		if (n % i == 0)
 		{
 			n /= i;
-			i += 2;
 		}
+		i += 2;
 	}
 	printf("%ld\n", n);
 	return (0);
