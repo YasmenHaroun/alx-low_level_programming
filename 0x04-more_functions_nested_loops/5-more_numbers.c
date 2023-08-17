f5eb80303c929937cb0b2d5b3d4e8ad29050281e
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-17 04:17:54 -0700

    try4 task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 11; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			if (i > 10)
+			if (i >= 10)
 			{
 				_putchar(i / 10 + '0');
 			}
