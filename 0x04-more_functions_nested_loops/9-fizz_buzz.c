14390d304db35f74a26eb84bad9ca8a56843f969
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-17 07:27:30 -0700

    try11 task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,7 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			if (num != 100)
+			if (i != 100)
 			{
 				printf("Buzz ");
 			}
@@ -29,7 +29,7 @@ int main(void)
 			{
 				printf("Buzz");
 			}
-		}
+	}
 		else
 		{
 			printf("%d ", i);
