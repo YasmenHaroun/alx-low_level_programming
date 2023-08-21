d1dec55ba62bbec22aaf65061a5ad322c03e4224
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-21 04:45:54 -0700

    try1 task9

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _atoi - convert string to integer
+ * @s: input string
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	char temp;
+	int i;
+
+	for (i = 0; i < strlength(s); i++)
+	{
+		if ()
+	}()
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,11 +2,20 @@
 
 /**
  * *_strcpy - copies the string pointed to by src
- * dest: destination buffer
- * src: source pionter
+ * @dest: output destination buffer
+ * @src: input source pionter
  *
  * Return: char
  */
 char *_strcpy(char *dest, char *src)
 {
+	int i = 0;
+
+	while (*src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
