2eefce8446ce06623ad7693c1820c25dcde64827
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-21 00:30:15 -0700

    try1 task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *@str: input string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int length;
+
+	length = strlen(str);
+	for (i = 0; i < length; i++)
+	{
+		_putchar(str(i));
+	}
+	_putchar('\n');
+}
