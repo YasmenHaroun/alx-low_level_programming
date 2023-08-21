b6ba75f36aa4f9f23dbfceda23c60736e6033161
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-21 03:32:32 -0700

    try 2 task8

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * dest: destination buffer
+ * src: source pionter
+ *
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
