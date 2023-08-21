384e657bc0d8923652cefdc12ae48b6126e69b7c
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-21 02:23:41 -0700

    try1 task6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of string
+ * @str: input string
+ *
+ * Return:void
+ */
+void puts_half(char *str)
+{
+	int len;
+	int i;
+
+	len = strlen(str);
+	if (len / 2 != 0)
+	{
+		i = (len -1) / 2;
+	}
+	else
+	{
+		i = len /  2;
+	}
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
