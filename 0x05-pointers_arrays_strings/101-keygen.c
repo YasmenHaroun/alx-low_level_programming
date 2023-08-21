3907497bd0ca397f8cf07e37aa54ef39e3bef514
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-21 16:37:11 -0700

    try1 task11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,8 +1,18 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main -  generate random password for 101-crackme
  *
  * Return: void
  */
-int man ()
+char main(void)
+{
+	int n;
+	/*Generate random numer*/
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	return (n);
+}
