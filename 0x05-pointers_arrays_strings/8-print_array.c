dfeb01310092f4ecee4048444a2da3253270151d
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-21 03:49:49 -0700

    try5 task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,7 +10,7 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	if ( n != 1)
 	{
 		for (i = 0; i < n; i++)
@@ -24,6 +24,7 @@ void print_array(int *a, int n)
 	else
 	{
 		printf("\n");
+		printf("\n";
 		printf("\n");
 	}
 }
