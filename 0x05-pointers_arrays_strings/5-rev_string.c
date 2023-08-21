f8bd11f8a6832d171537ca754d82ee82e39595ac
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-21 00:50:51 -0700

    try1 task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string -  reverse a string
+ * @s: input string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char temp_str[];
+	int l;
+	int i = 0;
+
+	l = strlen(s);
+	while (l != 0)
+	{
+		temp_str[i] = s[l];
+		l--;
+		i++;
+	}
+	strcpy(s, temp_str);
+}
