293ecb653075683498d63fe3f7153c5c10679aa4
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-23 08:33:43 -0700

    try1 task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,16 +15,16 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i] != '\0')
 	{
-		i++
+		i++;
 	}
-	for ( j = 0; j < n; j++)
+	for (j = 0; j < n; j++)
 	{
 		if (src[j] != '\0')
 		{
-			dest[(i+j)] = src[j];
+			dest[(i + j)] = src[j];
 		}
 	}
-	dest[(i+j)] = '\0';
+	dest[(i + j)] = '\0';
 
 	return (dest);
 }
