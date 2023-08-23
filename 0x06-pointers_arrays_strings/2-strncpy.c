37cce582b61d1f262f7f1b3e1aee7a36f771abfd
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-23 10:48:12 -0700

    try6

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
-	if (n > 0)
+	
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (src[i] != '\0')
-				dest[i] = src[i];
-			else if (src[i] != '\0')
-				dest[i] = '\0';
-		}
+		if (src[i] != '\0')
+			dest[i] = src[i]
+		else
+			dest[i] = '\0';
 	}
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,10 +12,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int temp;
 
-	for (i = 0; i < (n/2); i++)
+	for (i = 0; i < (n / 2); i++)
 	{
-		temp = a[n-1-i];
-		a[n-1-i] = a[i];
+		temp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
 		a[i] = temp;
-	}	
+	}
 }
