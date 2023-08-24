d9c5ba15ad671fbdcf179fcbc82f79dccf7b786d
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-24 00:18:46 -0700

    task6 try1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,23 +2,26 @@
 
 /**
  * cap_string -  capitalizes all words of a string
- * @str: input string
+ * @s: input string
  *
  * Return: char
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i) == ' ' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '?' || s[i] == '"' || s[i] == ')' || s[i] == '(' || s[i] == '{' || s[i] == '}' || s[i] == '!')
-		{		
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] -= 32;
+		else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+			|| s[i] == ';' || s[i] == '.' || s[i] == '?' || s[i] == '"'
+			|| s[i] == ')' || s[i] == '(' || s[i] == '{' || s[i] == '}'
+			|| s[i] == '!')
+		{
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
-			{
-				s[i + 1] -= s[i] - 32;
-			}
+				s[i + 1] -= 32;
 		}
 	}
-	return (str);
+	return (s);
 }
