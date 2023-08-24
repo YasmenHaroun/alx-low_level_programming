6a04d0777a163e310b0ecd1d523e04ea2bc25acb
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-23 23:28:42 -0700

    try10

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, flag;
 
-	for (i = 0; (s1[i] != '\0' || s2[i] != '\0'); i++)
+	for (i = 0; (s1[i] != '\0' && s2[i] != '\0'); i++)
 	{
-		if (s1[i] == s2[i])
-			flag = 0;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-	if (s1[i] == '\0' && s2[i] != '\0')
-		flag = -1;
-	else if (s1[i] != '\0' && s2[i] == '\0')
-		flag = 1;
-	return (flag);
+	return (0);
 }
