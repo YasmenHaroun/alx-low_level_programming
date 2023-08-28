e216f13fbca73f07340b5ef4e26f11ae9f00cd86
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-28 16:45:45 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,28 +9,25 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+        int i = 0;
 	int j;
 
+        if (needle[i] == NULL)
+                return (haystack);
 
-	if (needle == NULL)
-		return (haystack);
-	i = 0;
-	while (haystack[i] != '\0')
-	{
-		if (haystack[i] == needle[0])
-		{
-			j = 0;
-			while (needle[j] != '\0')
-			{
-				if (haystack[i + j] != needle[j])
-					break;
-				j++;
-			}
-			if (needle[j] == '\0')
-				return (haystack + i);
-		}
-		i++;
-	}
-	return (0);
+        for (i = 0; haystack[i] != '\0'; i++)
+        {
+                if (haystack[i] == needle[0])
+                {
+                        for (j = 0; needle[j] != '\0'; j++)
+                        {
+                                if (haystack[i + j] != needle[j])
+                                        break;
+                        }
+                        if (needle[j] == '\0')
+                                return (haystack + i);
+                }
+        }
+        return (0);
+}
 }
