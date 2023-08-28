830817ff7a49b1ad8e65ad367a8bec714c087b52
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-28 02:40:32 -0700

    try1 task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		if (s[i] != c)
+		if (s != c)
 			s++;
 		else
 			return (s);
 	}
 	if (c == '\0')
 		return (s);
+
 	return (NULL);
 }
