d6f737f9b97af862e39b40256871cc486cb62e35
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-30 04:33:52 -0700

    task3

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return natural square root
+ * @n: input number
+ *
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	else if (n * n == n)
+		return (n);
+	else
+		_sqrt_recursion(n - 1);
+}
