fbb97dea1975dd969011dc922e6796c4d73bf7cc
YasmenHaroun <eng.yasmen@gmail.com>
2023-08-30 05:32:35 -0700

    task3

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,18 +2,19 @@
 
 /**
  * _sqrt_recursion - return natural square root
+ * sqrt_2inputs - funcation take offset
  * @n: input number
- *
+ * @num: input number
  * Return: integer
  */
-int sqrt_2inputs( int num, int n)
+int sqrt_2inputs(int num, int n)
 {
 	if (n <= 0)
-                return (-1);
+		return (-1);
 	else if (n == 1)
 		return (1);
-        else if (n * n == num)
-                return (n);
+	else if (n * n == num)
+		return (n);
 	else
 		return (sqrt_2inputs(num, n + 1));
 }
