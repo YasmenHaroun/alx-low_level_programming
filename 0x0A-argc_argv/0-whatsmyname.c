0bf1b19af8082fb7c0fa1974e4e624e247056c83
YasmenHaroun <eng.yasmen@gmail.com>
2023-09-01 23:08:41 -0700

    try1 task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - main of program
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: zero
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
