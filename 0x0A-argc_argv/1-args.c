e3636575a1066ce70e5b3745025226bcad1323d9
YasmenHaroun <eng.yasmen@gmail.com>
2023-09-01 23:19:25 -0700

    try1 task0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main of program
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
